f0c4dc945700cdc6ddf27eff3087082e50b769e7
Valentas <valentas.saugunas@gmail.com>
2018-10-03 17:18:51 +0300

    praitos 3 uzduotys

---
+++ 7 uzduotis/javas.js
@@ -0,0 +1,9 @@
+$(function()
+{
+    $('.slider').on('input change', function(){
+        $(this).next($('.slider_label')).html(this.value);
+    });
+    $('.slider_label').each(function(){
+        var value = $(this).prev().attr('value');
+        $(this).html(value);
+    });}
